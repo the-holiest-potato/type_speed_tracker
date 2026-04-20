de544e706dde9958d85975b73bbc738bd6812124
the-holiest-potato <anushakaprasad12@gmail.com>
2026-04-20 20:40:17 +0530

    added SSL

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -7,6 +7,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 export const db = drizzle(pool, { schema });
